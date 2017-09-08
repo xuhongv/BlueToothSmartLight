b740a57362fedb571fddd30e948a496bfbd8fd0b
半颗心脏 <870189248@qq.com>
2017-09-08 12:07:07 +0800

    设备详情。

--- app/src/main/java/com/xuhong/csdn_bluetooth_master/MainActivity.java
+++ app/src/main/java/com/xuhong/csdn_bluetooth_master/MainActivity.java
@@ -1,22 +1,23 @@
 package com.xuhong.csdn_bluetooth_master;
 
-import android.app.Dialog;
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.xuhong.csdn_bluetooth_master.ui.BaseActivity;
 
 import java.io.IOException;
 import java.util.UUID;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 
 public class MainActivity extends BaseActivity {
@@ -30,25 +31,28 @@ public class MainActivity extends BaseActivity {
     private static final String SPP_UUID = "00001101-0000-1000-8000-00805F9B34FB";
 
     //ui
-    private Dialog progressDialog;
+    private SweetAlertDialog progerssAlertDialog;
     private Context mContext;
 
-    private static final int HANDLER_SUCCEED= 201;
-    private static final int HANDLER_FAIL= 202;
+    private static final int HANDLER_SUCCEED = 201;
+    private static final int HANDLER_FAIL = 202;
 
-    private Handler mHandler =new Handler(){
+    private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case HANDLER_SUCCEED:
-                    progressDialog.dismiss();
+                    progerssAlertDialog.dismiss();
+                    Toast.makeText(mContext, "连接成功", Toast.LENGTH_SHORT).show();
                     break;
                 case HANDLER_FAIL:
-                    progressDialog.show();
+                    progerssAlertDialog.dismiss();
+                    showFailAlertDialog("连接失败");
                     break;
             }
         }
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,9 +64,15 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initView() {
-        progressDialog = new Dialog(this);
-        progressDialog.setTitle("正在连接...");
-        progressDialog.show();
+        progerssAlertDialog = new SweetAlertDialog(mContext, SweetAlertDialog.PROGRESS_TYPE);
+        progerssAlertDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.yellow));
+        progerssAlertDialog.setTitleText("正在连接...");
+        progerssAlertDialog.show();
+        //显示返回按钮
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //取消阴影
+        getSupportActionBar().setElevation(0);
+
     }
 
     @Override
@@ -77,11 +87,11 @@ public class MainActivity extends BaseActivity {
             mBluetoothDevice = bundle.getParcelable("device");
             if (mBluetoothDevice != null) {
                 Log.e("BaseActivity", "收到的蓝牙名字：" + mBluetoothDevice.getName());
+                setTitle(mBluetoothDevice.getName());
                 new mThread(mBluetoothDevice).start();
             }
         }
     }
-
 
     //内部类子线程
     private class mThread extends Thread {
@@ -95,25 +105,18 @@ public class MainActivity extends BaseActivity {
         @Override
         public void run() {
             UUID uuid = UUID.fromString(SPP_UUID);
-            Log.e("BaseActivity", "准备连接");
             try {
                 mSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
                 mSocket.connect();
                 mHandler.sendEmptyMessage(HANDLER_SUCCEED);
-                Log.e("BaseActivity", "连接succeed:");
             } catch (IOException e) {
                 mHandler.sendEmptyMessage(HANDLER_FAIL);
-                Log.e("BaseActivity", "连接失败:" + e);
             }
         }
     }
 
-
-
-
     @Override
     protected void succeedBindBTDevices() {
-        Log.e("BaseActivity", "succeedBindBTDevices");
         mHandler.sendEmptyMessage(HANDLER_SUCCEED);
     }
 
@@ -121,23 +124,86 @@ public class MainActivity extends BaseActivity {
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver();
-
-
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     if (mSocket != null) {
                         mSocket.close();
-                        mSocket=null;
-                        Log.e("BaseActivity", "关闭成功");
+                        mSocket = null;
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.e("BaseActivity", "关闭失败");
                 }
             }
         }).start();
 
     }
+
+    private void showFailAlertDialog(String message) {
+        SweetAlertDialog failAlertDialog = new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE);
+        failAlertDialog.setCustomImage(R.mipmap.ic_launcher);
+        failAlertDialog.setTitleText(message);
+        failAlertDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                sweetAlertDialog.dismiss();
+                finish();
+            }
+        });
+        failAlertDialog.show();
+    }
+
+    private void showDevicesDetailInf() {
+        SweetAlertDialog mAlertDialog = new SweetAlertDialog(this, SweetAlertDialog.NORMAL_TYPE);
+        mAlertDialog.setTitleText("设备信息");
+        mAlertDialog.setContentText("设备地址：" + mBluetoothDevice.getAddress() +"\n设备名字：" + mBluetoothDevice.getName());
+        mAlertDialog.show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //关闭当前Activity
+        switch (item.getItemId()) {
+            case R.id.menu_detial:
+                showDevicesDetailInf();
+                break;
+            case R.id.menu_rename:
+                break;
+            case android.R.id.home:
+                new SweetAlertDialog(this, SweetAlertDialog.NORMAL_TYPE)
+                        .setTitleText("提示")
+                        .setContentText("确定要退出控制该设备吗？")
+                        .setCancelText("取消")
+                        .setConfirmText("确定")
+                        .showCancelButton(true)
+                        .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.cancel();
+                            }
+                        })
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                finish();
+                            }
+                        })
+                        .show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_devices, menu);
+        return true;
+    }
+
+
 }
